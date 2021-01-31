b38b040a228af1f2b67cb664035a3c8e04e48b0b
benjamin-kromer <benjamin.kromer1986@gmail.com>
2021-01-31 19:17:20 +0100

    added img & contact form

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 //      DEPENDENCIES AND IMPORTS
 //==========================================
 require('dotenv').config();
+const ejs = require('ejs');
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 //==========================================
 
 //==========================================
@@ -28,10 +30,48 @@ app.use(bodyParser.json())
 //               ROUTES
 //==========================================
 app.get('/',(req,res)=>{
-    res.render('index')
-
+    res.render('index') 
+})
+app.post('/',(req,res)=>{
+    console.log(req.body);
+    sendEmail(req.body.email, req.body.message)
+    .then((response)=>{console.log("Antwort: "+response)})
+    .catch((err)=>{console.log("Fehler: "+err)})
+    res.redirect("/");
+})
 //==========================================
 const listener = app.listen(process.env.PORT || 3000, function() {
     console.log("Server started on port "+listener.address().port);
   });
 //==========================================
+
+
+//==========================================
+//          FUNCTIONS
+//==========================================
+async function sendEmail(mail,msg){
+    var config = {
+      method: 'post',
+      url : `https://api.sendgrid.com/v3/mail/send`,
+      headers: {'Content-Type': 'application/json','Accept':'application/json','Authorization':`Bearer ${process.env.SEND_GRID_TOKEN}`},
+      data: {
+        personalizations: [{
+                to: [{
+                        email: mail
+                    }]
+            }],
+        from: {
+            email: mail
+        },
+        subject: "New Contact",
+        content: [{
+                type: "text/html",
+                value: "msg"
+            }]
+    
+    }}
+    const promSendMail = axios(config);
+    const sendGridMailres = await promSendMail;
+    return sendGridMailres
+  }
+  //==========================================
